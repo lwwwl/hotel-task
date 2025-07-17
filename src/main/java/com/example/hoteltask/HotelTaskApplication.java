4838e45c5ddd998199e906e9a325310fa4eb5ca4
lao-laptop <lwggcn@gmail.com>
2025-07-17 15:08:08 +0800

    init

---
+++ src/main/java/com/example/hoteltask/HotelTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoteltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hoteltask/HotelTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoteltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
