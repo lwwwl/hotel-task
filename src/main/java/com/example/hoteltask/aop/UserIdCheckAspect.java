f2b1430c3ea89bea49329a0ab1c2a604f1d48e51
lao-desktop <lwggcn@gmail.com>
2025-07-24 11:35:26 +0800

    add

--- src/main/java/com/example/hoteltask/aop/UserIdCheckAspect.java
+++ src/main/java/com/example/hoteltask/aop/UserIdCheckAspect.java
@@ -1,6 +1,5 @@
 package com.example.hoteltask.aop;
 
-import com.example.hoteltask.aop.annotation.RequireUserId;
 import com.example.hoteltask.model.response.ApiResponse;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -20,7 +19,7 @@ public class UserIdCheckAspect {
     @Around("@within(com.example.hoteltask.aop.annotation.RequireUserId) || @annotation(com.example.hoteltask.aop.annotation.RequireUserId)")
     public Object checkUserIdHeader(ProceedingJoinPoint joinPoint) throws Throwable {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-        String userId = request.getHeader("userId");
+        String userId = request.getHeader("X-User-Id");
 
         if (userId == null || userId.isEmpty()) {
             return ResponseEntity
